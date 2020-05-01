c3a252d5cc8ea2405618ac1d05c5a0755bc9121b
motovmp1 <vinicius.mpinho@gmail.com>
2020-05-01 18:00:40 +0800

    lenovo 5

--- TestCases/teste.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-ECHO Congratulations! Your test started successfully.
-ECHO -------------------------------------------
-cd /Users/elsys/PycharmProjects/Robot_test/TestCases
-robot inputbox.robot
